47494e88d21aca9eefbe3d1ecaf5fa1788294e05
Trent Henderson <trenthenderson@Trents-MacBook-Pro-2.local>
2020-09-29 18:05:05 +1000

    Edited webtool

--- webtool/server.R
+++ webtool/server.R
@@ -5,33 +5,6 @@ shinyServer <- function(input, output, session) {
 #------------------------TIME SERIES------------------------------
   
   #-------------------
-  # RAW DATA
-  #-------------------
-  
-  output$raw_ts <- renderPlotly({
-
-    p <- d1 %>%
-      filter(crash_severity == input$ts_severity) %>%
-      group_by(crash_date, crash_severity) %>%
-      summarise(value = sum(value)) %>%
-      ungroup() %>%
-      ggplot(aes(x = crash_date, y = value)) +
-      geom_line(size = 1.25, colour = "steelblue2") +
-      geom_point(size = 1.5, colour = "steelblue2") +
-      labs(x = "Date",
-           y = "Count") +
-      scale_y_continuous(labels = comma) +
-      theme_bw() +
-      theme(panel.grid.minor = element_blank())
-    
-    ggplotly(p) %>%
-      layout(plot_bgcolor  = "rgba(255, 255, 255, 0.2)",
-             paper_bgcolor = "rgba(255, 255, 255, 0.2)") %>%
-      config(displayModeBar = F)
-    
-  })
-  
-  #-------------------
   # DECOMPOSITION
   #-------------------
   
